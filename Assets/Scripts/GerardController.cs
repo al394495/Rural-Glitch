a1a8ff6c6b0bd52217afbc330c9a7650e5573bb3
moggito <90263325+moggichita@users.noreply.github.com>
2021-10-22 13:01:06 +0200

    Programar movimiento horizontal

--- Assets/Scripts/GerardController.cs
+++ Assets/Scripts/GerardController.cs
@@ -2,13 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class RubyController : MonoBehaviour
+public class GerardController : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
     {
-     
-
+        
     }
 
     // Update is called once per frame
@@ -16,8 +15,8 @@ public class RubyController : MonoBehaviour
     {
         float horizontal = Input.GetAxis("Horizontal");
         Vector2 position = transform.position;
-        position.x = position.x + 0.1f * horizontal * Time.deltaTime;
+        position.x = position.x + 3.0f * horizontal * Time.deltaTime;
         transform.position = position;
     }
-
 }
+
