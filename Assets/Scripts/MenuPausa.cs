63d16c976566f29cde916001cc646bbd8a89edaa
al394495 <al394495@uji.es>
2021-10-24 22:41:00 +0200

    menu movil

---
+++ Assets/Scripts/MenuPausa.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuPausa : MonoBehaviour
+{
+    public GameObject Imagen;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (Time.timeScale == 1.0f)
+            {
+                Time.timeScale = 0.0f;
+                Imagen.SetActive(true);
+            }    
+            else
+            {
+                Time.timeScale = 1.0f;
+                Imagen.SetActive(false);
+            }
+        }
+    }
+}
