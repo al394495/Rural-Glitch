3392f7becd5df9deaa17987560c1e18b9f2bc1c5
AlvaroSbsn <78981404+AlvaroSbsn@users.noreply.github.com>
2021-10-26 15:19:51 +0200

    hud finished

--- Assets/Scripts/hud.cs
+++ Assets/Scripts/hud.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class hud : MonoBehaviour
 {
+    private float amount = 500.0f;
+    private bool mensaje = false;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,17 +16,25 @@ public class hud : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKeyDown(KeyCode.K))
+        if (Input.GetKeyDown(KeyCode.K))
         {
-            do
-            {
-                vibrar();
-            } while (!menuabierto);
+            if (mensaje == false)
+                mensaje = true;
+            else
+                mensaje = false;
+        }
+        if (mensaje == true)
+        {
+            vibrar();
         }
     }
 
     void vibrar()
     {
-
+        Vector3 newPos = Random.insideUnitSphere * (Time.deltaTime * amount);
+        newPos.y = transform.position.y;
+        newPos.z = transform.position.z;
+        transform.position = newPos;
     }
 }
+
