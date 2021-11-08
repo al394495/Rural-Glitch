fabe22e87a6f4b567bc5f134eead1fe62b367aba
al394495 <al394495@uji.es>
2021-11-08 06:52:14 +0100

    cambiar condicion de cambio de escena

--- Assets/Scripts/CambioEscena.cs
+++ Assets/Scripts/CambioEscena.cs
@@ -5,9 +5,10 @@ using UnityEngine.SceneManagement;
 
 public class CambioEscena : MonoBehaviour
 {
+    public GameObject yaya;
     private void OnTriggerEnter2D(Collider2D other)
     {
-        if (other.tag == "Player")
+        if (other.tag == "Player" && yaya.GetComponent<InteraccionYaya>().dialogo == true)
         {
             SceneManager.LoadScene("Bosque"); 
         }
