6cdec7d683a0841aef527bae38cb92a4342b33fa
moggito <90263325+moggichita@users.noreply.github.com>
2021-11-13 17:38:35 +0100

    Programar interacción con un boton y que actualice el contador robellons

--- Assets/Scripts/robello.cs
+++ Assets/Scripts/robello.cs
@@ -1,18 +1,62 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class robello : MonoBehaviour {
-    private bool isCollected = false;
 
-    public bool Collect()
+    public Text ContadorRobello;
+
+    private int robellons = 0;
+
+    [SerializeField]
+    private Text pickUpText;
+
+    private bool pickUpAllowed;
+
+    // Use this for initialization
+    private void Start()
     {
-        if (isCollected)
-            return false;
-
-        isCollected = true;
-        Destroy(gameObject);
-        return true;
+        pickUpText.gameObject.SetActive(false);
     }
 
+    // Update is called once per frame
+    private void Update()
+    {
+        if (pickUpAllowed && Input.GetKeyDown(KeyCode.E))
+            PickUp();
+            
+            
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.name.Equals("Gerard"))
+        {
+            pickUpText.gameObject.SetActive(true);
+            pickUpAllowed = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.name.Equals("Gerard"))
+        {
+            pickUpText.gameObject.SetActive(false);
+            pickUpAllowed = false;
+        }
+    }
+
+    private void PickUp()
+    {
+        robellons++;
+        UpdateGUI();
+        Destroy(gameObject);
+    }
+
+    private void UpdateGUI()
+    {
+        ContadorRobello.text = robellons.ToString();
+    }
 }
