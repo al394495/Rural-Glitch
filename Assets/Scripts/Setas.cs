321e4849efd3cb269634aefe70ad07e2164986cb
moggito <90263325+moggichita@users.noreply.github.com>
2021-11-14 18:42:46 +0100

    Programar interacción con el resto de setas

---
+++ Assets/Scripts/Setas.cs
@@ -0,0 +1,66 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Setas : MonoBehaviour
+{
+    [SerializeField]
+    private Text pickUpText;
+    [SerializeField] 
+    private Image Bocadillo;
+    private bool pickUpAllowed;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        pickUpText.gameObject.SetActive(false);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (pickUpAllowed && Input.GetKeyDown(KeyCode.E))
+        {
+            Bocadillo.gameObject.SetActive(true);
+        }
+
+        if  (pickUpAllowed == false)
+        {
+            Bocadillo.gameObject.SetActive(false);
+        }
+
+
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+
+        if (collision.gameObject.tag == "NonCollectible")
+        {
+           
+            pickUpText.gameObject.SetActive(true);
+            pickUpAllowed = true;
+
+
+        }
+
+
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+
+        if (collision.gameObject.tag == "NonCollectible")
+        {
+            pickUpText.gameObject.SetActive(false);
+            pickUpAllowed = false;
+
+        }
+
+
+    }
+
+
+}
