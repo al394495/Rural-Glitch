8c55a74dbec0cb54afed9f4ff8e75464d2c55bb9
AlvaroSbsn <78981404+AlvaroSbsn@users.noreply.github.com>
2021-11-23 13:04:46 +0100

    NoFuncionaPeroPodria

--- Assets/Scripts/Mover.cs
+++ Assets/Scripts/Mover.cs
@@ -6,6 +6,7 @@ public class Mover : MonoBehaviour
 {
 
     private bool estaMoviendose;
+    public GameObject Moneda;
 
     public void OnMouseDown()
     {
@@ -24,5 +25,11 @@ public class Mover : MonoBehaviour
             Vector2 posicionRaton = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;
             transform.Translate(posicionRaton);
         }
+    }
+
+    public void OnTriggerEnter2D(Collider2D other)
+    {
+        Debug.Log("XD");
+        Moneda.GetComponent<Renderer>().enabled = false;
     }
 }
