e62fabb6b4e82c739cdfa3fbd4662663c69c29ab
al395603 <al395603@uji.es>
2021-11-28 22:36:22 +0100

    Actualizar vibración móvil según misiones

--- Assets/Scripts/MenuPausa.cs
+++ Assets/Scripts/MenuPausa.cs
@@ -36,6 +36,7 @@ public class MenuPausa : MonoBehaviour
                     botonconfiguracion.SetActive(true);
                     botonsalida.SetActive(true);
                     menuabierto = true;
+                    hud.mensaje = false;
                 }
                 else
                 {
