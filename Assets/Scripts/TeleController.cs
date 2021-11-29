6e88a5067232003b3b30fc594faab6a6dff7feb8
moggito <90263325+moggichita@users.noreply.github.com>
2021-11-29 23:16:13 +0100

    Programar script de la tele y contador

--- Assets/Scripts/TeleController.cs
+++ Assets/Scripts/TeleController.cs
@@ -4,15 +4,29 @@ using UnityEngine;
 
 public class TeleController : MonoBehaviour
 {
+
+    public Animator anim;
+    public KeyCode click;
+    public int contador;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        contador = 0;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(click)) {
+
+            contador++;
+        }
+
+        if (contador == 5) {
+
+            anim.SetTrigger("TeleTrigger");
+        }
+
     }
 }
