341ef96d21f1510bf7a7f2ec6c5c36f63ae49a40
al394495 <al394495@uji.es>
2021-11-30 04:57:44 +0100

    programar dialogos con los amigos

---
+++ Assets/Scripts/ScriptsCalleFuente/InteraccionEmo.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InteraccionEmo : MonoBehaviour
+{
+    public GameObject Texto;
+    public bool cerca = false;
+    public bool dialogo = false;
+    public GameObject Menu;
+
+    void Update()
+    {
+        if (cerca)
+        {
+            if (Input.GetKeyDown(KeyCode.Space) && Menu.GetComponent<MenuPausa>().menuabierto == false)
+            {
+                FindObjectOfType<DialogueTriggerEmo>().TriggerDialogue();
+                dialogo = true;
+            }
+        }
+
+        if (dialogo && Input.GetKeyDown(KeyCode.E))
+        {
+            FindObjectOfType<DialogueManager>().DisplayNextSentence();
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        Texto.SetActive(true);
+        cerca = true;
+
+    }
+
+    void OnTriggerExit2D(Collider2D other)
+    {
+        Texto.SetActive(false);
+        cerca = false;
+    }
+}
