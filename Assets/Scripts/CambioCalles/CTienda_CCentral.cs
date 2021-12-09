54d946d900bcf90b718e19a1c86b4792ce76879c
al395603 <al395603@uji.es>
2021-12-09 02:55:55 +0100

    Actualizar escena calle tienda

--- Assets/Scripts/CambioCalles/CTienda_CCentral.cs
+++ Assets/Scripts/CambioCalles/CTienda_CCentral.cs
@@ -6,15 +6,39 @@ using UnityEngine.SceneManagement;
 public class CTienda_CCentral : MonoBehaviour
 {
     public GameObject Texto;
+    public GameObject Barrera;
+    public bool cerca = false;
+    public GameObject Menu;
+
+    void Update()
+    {
+        if (VariablesGlobales.dia == 3)
+        {
+            Barrera.SetActive(false);
+            if (cerca)
+            {
+                if (Input.GetKeyDown(KeyCode.Space) && Menu.GetComponent<MenuPausa>().menuabierto == false)
+                {
+                    MainMenuScript.cambio = "calleTienda-central";
+                    //SceneManager.LoadScene("Central");
+                    Debug.Log("Entra central");
+                }
+            }
+        }
+    }
 
     void OnTriggerEnter2D(Collider2D other)
-    {
-        Texto.SetActive(true);
-
+    {       
+        if (VariablesGlobales.dia == 3)
+        {
+            Texto.SetActive(true);
+            cerca = true;
+        }
     }
 
     void OnTriggerExit2D(Collider2D other)
     {
         Texto.SetActive(false);
+        cerca = false;
     }
 }
