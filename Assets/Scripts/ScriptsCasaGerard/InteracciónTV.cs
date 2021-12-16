26b29897c5f12d03893e3a6f1017fa84df56a41b
al394495 <al394495@uji.es>
2021-12-16 05:47:16 +0100

    aun mas cosas

--- Assets/Scripts/ScriptsCasaGerard/InteracciónTV.cs
+++ Assets/Scripts/ScriptsCasaGerard/InteracciónTV.cs
@@ -24,7 +24,7 @@ public class InteracciónTV : MonoBehaviour
 
     void OnTriggerEnter2D(Collider2D other)
     {
-        if (VariablesGlobales.amigos == 3 && VariablesGlobales.dia == 2)
+        if ((VariablesGlobales.amigos == 3 && VariablesGlobales.dia == 2) && !VariablesGlobales.minijuegoRealizado3)
         {
             Texto.SetActive(true);
             cerca = true;
