1c6b88bee1352911d41bac9131fd78a0eefc3c07
al395603 <al395603@uji.es>
2022-01-22 21:36:08 +0100

    Mejorar escena botón

--- Assets/Scripts/Central/BotonController.cs
+++ Assets/Scripts/Central/BotonController.cs
@@ -7,7 +7,9 @@ using UnityEngine.SceneManagement;
 public class BotonController : MonoBehaviour
 {
     public GameObject Imagen;
-    Animator animator;
+    public Animator animator;
+    public float tiempoIni = 0;
+    public float tiempoFin = 3;
 
     public GameObject cinematicaFinal;
     public VideoPlayer videoFinal;
@@ -17,17 +19,28 @@ public class BotonController : MonoBehaviour
         animator = Imagen.GetComponent<Animator>();
         cinematicaFinal.SetActive(false);
     }
+    private void Update()
+    {
+        
+    }
     public void PulsarBoton()
     {
         animator.SetBool("Pulsado", true);
-        cinematicaFinal.SetActive(true);
-        videoFinal = cinematicaFinal.GetComponent<VideoPlayer>();
-        videoFinal.Play();
-        videoFinal.loopPointReached += CheckOver;
+        
+
+
     }
 
     void CheckOver(VideoPlayer vp)
     {
         SceneManager.LoadScene("Menú");
+    }
+
+    void Cinematica()
+    {
+        cinematicaFinal.SetActive(true);
+        videoFinal = cinematicaFinal.GetComponent<VideoPlayer>();
+        videoFinal.Play();
+        videoFinal.loopPointReached += CheckOver;
     }
 }
